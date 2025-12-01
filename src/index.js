99b4a4712956b4c6f474221f04c297e664b1cc98
Artem Igonin <officialigonin@mail.ru>
2025-12-01 18:05:59 +0400

    correct msgs and identify

--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ const root = () => {
 
   const wsServer = new WebSocketServer({port: BACK_PORT});
   console.log(wsServer.options.port, " port ");
+  wsServer["clientsUnicId"] = [];
   wsServer.on("connection", ws => {
-    console.log("anybody has connected");
+    const newClientId = new Date().getMilliseconds();
+    console.log("anybody has connected", newClientId);
+
+    wsServer["clientsUnicId"] = [...wsServer?.clientsUnicId, newClientId];
+    ws.send(
+      JSON.stringify({
+        wsServer: "client identify",
+        text: `Сервер приветствует тебя!\n\ Твой id ${newClientId} //c:WS`,
+        clientId: newClientId,
+      }),
+    ); // Приветствие сервера при первом подключении. Можно настроить авторизацию и вернуть клиенту его айдишник
     ws.on("message", msg => {
       wsServer.clients.forEach(client => {
+        console.log(String(msg), " cur msg");
         if (client.readyState === WebSocket.OPEN) client.send(String(msg));
       });
     });
-    ws.on("close", test => {
-      console.log("Any client disconected", test);
+    ws.on("close", (test, reason) => {
+      console.log("Any client disconected");
     });
   });
 };
